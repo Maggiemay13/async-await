3abaaefb5650fc9c074d2a75a6415fe11cbe987a
maggie <margaret13.may@gmail.com>
2023-09-10 01:10:31 -0500

    add files

---
+++ part-1/app.js
@@ -0,0 +1,25 @@
+let favNumber = 14;
+let baseURL = "http://numbersapi.com";
+
+async function part1() {
+  let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+  console.log(data);
+}
+part1();
+
+const favNumbers = [27, 63, 15];
+async function part2() {
+  let data = await $.getJSON(`${baseURL}/${favNumbers}?json`);
+  console.log(data);
+}
+part2();
+
+async function part3() {
+  let facts = await Promise.all(
+    Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
+  );
+  facts.forEach((data) => {
+    $("body").append(`<p>${data.text}</p>`);
+  });
+}
+part3();
